d2441847873da2a82982386191568c7db74fe87b
MawuliAgamah <64679996+MawuliAgamah@users.noreply.github.com>
2024-08-25 21:26:32 +0100

    setting up tailwind

---
+++ app/main.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+import uvicorn  
+
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.gzip import GZipMiddleware
+
+# Set up the base directory and path to templates
+BASE_DIR = Path(__file__).resolve().parent
+
+
+
+# initialise the fast api application
+app = FastAPI()
+app.mount("/static", StaticFiles(directory=BASE_DIR/'static'), name="static")
+app.add_middleware(GZipMiddleware)
+
+templates = Jinja2Templates(directory=BASE_DIR / 'templates')
+
+# Define the route for the home page
+@app.get('/', response_class=HTMLResponse)
+async def index(request: Request):
+    posts = [
+        {"id": 1, "title": "fastapi.blog title 1", "body": "Learn FastAPI with the fastapi.blog team 1"},
+        {"id": 2, "title": "fastapi.blog title 2", "body": "Learn FastAPI with the fastapi.blog team 2"},
+        {"id": 3, "title": "fastapi.blog title 3", "body": "Learn FastAPI with the fastapi.blog team 3"},
+    ]
+    context = {
+        "request": request,
+        "posts": posts,
+        "title": "Home Page"
+    }
+    return templates.TemplateResponse("index.html", context)
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
